1a4d44136e7274c04ba3d53b338af1a1843b86c1
Clément Laigle <c.laigle@catie.fr>
2024-03-29 13:43:20 +0100

    Add and run pre-commit

--- ili9163c/ili9163c.h
+++ ili9163c/ili9163c.h
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 2020, CATIE
  * SPDX-License-Identifier: Apache-2.0
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,13 +17,12 @@
 #ifndef CATIE_SIXTRON_ILI9163C_H_
 #define CATIE_SIXTRON_ILI9163C_H_
 
-#include "mbed.h"
 #include "ili9xxx.h"
+#include "mbed.h"
 
 namespace sixtron {
 
-class ILI9163C : public ILI9XXX
-{
+class ILI9163C: public ILI9XXX {
 public:
     ILI9163C(SPI *spi, PinName cs, PinName dcx, PinName backlight);
     void init();
@@ -32,4 +31,3 @@ public:
 } // namespace sixtron
 
 #endif // CATIE_SIXTRON_ILI9163C_H_
-
